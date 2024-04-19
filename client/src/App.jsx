abdcc6002c74e9ae697eea48c16dde569bc631e3
Andry Ariadi <andryariadi23@gmail.com>
2024-04-19 19:59:24 +0700

    add register and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import HomePage from "./routes/HomePage/HomePage";
 
-import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./routes/layout/layout";
 import ListPage from "./routes/ListPage/ListPage";
 import DetailPage from "./routes/SinglePage/DetailPage";
 import ProfilePage from "./routes/ProfilePage/ProfilePage";
+import Login from "./routes/Login/Login";
+import Register from "./routes/Register/Register";
 
 function App() {
   const router = createBrowserRouter([
@@ -27,6 +29,14 @@ function App() {
         {
           path: "/profile",
           element: <ProfilePage />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
         },
       ],
     },
