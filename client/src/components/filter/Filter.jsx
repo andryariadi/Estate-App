940cdeae044fb075d2c4a05f616b26943d4bdc98
Andry Ariadi <andryariadi23@gmail.com>
2024-04-24 22:09:38 +0700

    setup filter in list page

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -1,24 +1,49 @@
+import { useSearchParams } from "react-router-dom";
 import "./filter.scss";
 import { IoSearchOutline } from "react-icons/io5";
+import { useState } from "react";
 
 export default function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    property: searchParams.get("property") || "",
+    minPrice: searchParams.get("minPrice") || "",
+    maxPrice: searchParams.get("maxPrice") || "",
+    bedroom: searchParams.get("bedroom") || "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setQuery((prevQuery) => ({
+      ...prevQuery,
+      [name]: value,
+    }));
+  };
+
+  const handleFilter = () => {
+    setSearchParams(query);
+  };
+
   return (
     <>
       <div className="filter">
         <h1>
-          Search result for <b>London</b>
+          Search result for <b>{query.city}</b>
         </h1>
         <div className="top">
           <div className="item">
             <label htmlFor="city">Location</label>
-            <input type="text" id="city" name="city" placeholder="City Location" />
+            <input type="text" id="city" name="city" value={query.city} onChange={handleChange} defaultValue={query.city} placeholder="City Location" />
           </div>
         </div>
         <div className="bottom">
           <div className="item">
             <label htmlFor="type">Type</label>
-            <select name="type" id="type">
-              <option selected disabled>
+            <select name="type" id="type" value={query.type} onChange={handleChange} defaultValue={query.type}>
+              <option value="" disabled>
                 Any
               </option>
               <option value="buy">Buy</option>
@@ -27,8 +52,8 @@ export default function Filter() {
           </div>
           <div className="item">
             <label htmlFor="property">Property</label>
-            <select name="property" id="property">
-              <option selected disabled>
+            <select name="property" id="property" value={query.property} onChange={handleChange} defaultValue={query.property}>
+              <option value="" disabled>
                 Any
               </option>
               <option value="apartment">Apartment</option>
@@ -39,17 +64,17 @@ export default function Filter() {
           </div>
           <div className="item">
             <label htmlFor="minPrice">Min Price</label>
-            <input type="number" id="minPrice" name="minPrice" placeholder="Any" />
+            <input type="number" id="minPrice" name="minPrice" value={query.minPrice} onChange={handleChange} defaultValue={query.minPrice} placeholder="Any" />
           </div>
           <div className="item">
             <label htmlFor="maxPrice">Max Price</label>
-            <input type="number" id="maxPrice" name="maxPrice" placeholder="Any" />
+            <input type="number" id="maxPrice" name="maxPrice" value={query.maxPrice} onChange={handleChange} defaultValue={query.maxPrice} placeholder="Any" />
           </div>
           <div className="item">
             <label htmlFor="bedroom">Bedroom</label>
-            <input type="text" id="bedroom" name="bedroom" placeholder="Any" />
+            <input type="text" id="bedroom" name="bedroom" value={query.bedroom} onChange={handleChange} defaultValue={query.bedroom} placeholder="Any" />
           </div>
-          <button>
+          <button onClick={handleFilter}>
             <div className="searchIcon">
               <IoSearchOutline size={28} />
             </div>
