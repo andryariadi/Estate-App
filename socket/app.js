b7cfa0f00f3fc1212a6e81f454240e3b78388461
Andry Ariadi <andryariadi23@gmail.com>
2024-04-29 13:37:53 +0700

    add socket context

--- socket/app.js
+++ socket/app.js
@@ -1,1 +1,13 @@
 import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:5173/",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket, "<---disocketapp");
+});
+
+io.listen(4000);
